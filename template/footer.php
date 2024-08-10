bffca396049e0125d50447a21f03c7c1bf299ae1
Jhon <jhonbyronquirozperez@gmail.com>
2024-08-10 15:59:00 -0500

    se  agregan las apis y el js

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,85 @@
 </div>
     </div>
 
+   
+
+
+
+
+
+
+<!-- Footer Api uselessfacts -->
+<footer class="bg-body-tertiary text-center text-lg-start">
+
+  
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+    <div id="fact">
+            Cargando un hecho interesante...
+    </div>
+     <br><br>
+
+    <a class="text-body" href="https://byroncodes.co/">Por: Byron © 2024</a>
+  </div>
+</footer>
+<!-- Footer Api uselessfacts -->
+
+
+
+
+
+
+<script>
+    // Solicitud para obtener un hecho aleatorio en inglés y mostrarlo en el footer
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/today?language=en', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la solicitud: ' + response.statusText);
+        }
+        return response.json(); // Convertir la respuesta a JSON
+    })
+    .then(data => {
+        document.getElementById('fact').textContent = `Hecho del día!!!: ${data.text}`;
+    })
+    .catch(error => {
+        document.getElementById('fact').textContent = 'Hubo un problema al cargar el hecho.';
+        console.error('Hubo un problema con la solicitud:', error);
+    });
+
+
+
+
+
+    // Realizar la solicitud a la API para obtener dos hechos sobre gatos en español
+    fetch('https://meowfacts.herokuapp.com/?count=2&lang=esp')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error en la solicitud: ' + response.statusText);
+                }
+                return response.json();  // Convertir la respuesta a JSON
+            })
+            .then(data => {
+                // Unir los hechos en un solo mensaje
+                const catFacts = data.data.join('\n');
+                // Mostrar la ventana emergente con los datos sobre gatos
+                alert('Sabías que...\n\n' + catFacts);
+            })
+            .catch(error => {
+                console.error('Hubo un problema con la solicitud:', error);
+            });
+
+
+
+
+    
+
+
+</script>
+
+
+
 </body>
 </html>
--- template/header.php
+++ template/header.php
@@ -16,11 +16,11 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <ul class="nav navbar-nav">
             <li class="nav-item ">
-                <a class="nav-link" href="#"><img src="https://ptm.com.co/wp-content/uploads/2023/04/INDEX-LOGO-264X80-1.png" alt="" width="50%"> </a>
+                <a class="nav-link" href="index.php"><img src="https://ptm.com.co/wp-content/uploads/2023/04/INDEX-LOGO-264X80-1.png" alt="" width="50%"> </a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="#">Inicio</a>
+                <a class="nav-link" href="index.php">Inicio</a>
             </li>
 
             <li class="nav-item">
